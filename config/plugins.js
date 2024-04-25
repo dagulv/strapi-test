2815936c4acaf83881a60fa9dfc5643867bf9ad7
dagulv <dagulvsback@gmail.com>
2024-04-25 16:46:02 +0200

    wip

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
       config: {
         owner: 'dagulv', // The Github organisation or user
         repo: 'strapi-test', // The name of the repository
-        workflow_id: 'test.yml', // The workflow_id or filename
+        workflow_id: 'deploy.yml', // The workflow_id or filename
         token: env('GITHUB_TOKEN'), // The GitHub personal access token with access to trigger workflows and view build status
         branch: 'main', // The branch the workflow should be triggered on
       }
