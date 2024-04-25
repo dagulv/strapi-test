6ad921d03017bc2be13f6eadf794f22dd01201ea
dagulv <dagulvsback@gmail.com>
2024-04-25 16:05:09 +0200

    Added github workflow

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'site-publisher': {
+      config: {
+        owner: 'dagulv', // The Github organisation or user
+        repo: 'strapi-test', // The name of the repository
+        workflow_id: 'test.yml', // The workflow_id or filename
+        token: env('GITHUB_TOKEN'), // The GitHub personal access token with access to trigger workflows and view build status
+        branch: 'master', // The branch the workflow should be triggered on
+      }
+    }
+  });
